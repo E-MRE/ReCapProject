8b6e37d5b7eaf482dadb7cbb73fc9cfd1cb38bfa
E-MRE <gltkr.emre@gmail.com>
2021-08-16 00:53:03 +0300

    12. Gün Ödev 1, Ödev 2 ve Ödev 3

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,5 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -19,14 +21,9 @@ namespace Business.Concrete
             this.carDal = carDal;
         }
 
+        [ValidationAspect(typeof(CarValidator))]
         public IResult Add(Car car)
         {
-            if (car.Description.Length < 2 && car.DailyPrice <= 0)
-                return new ErrorResult(Messages.CarNameInvalid);
-            else if (car.DailyPrice <= 0)
-                return new ErrorResult(Messages.DailyPriceInvalid);
-
-
             carDal.Add(car);
             return new SuccessResult(Messages.CarAdded);
         }
---
+++ Business/ValidationRules/FluentValidation/UserValidator.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class UserValidator : AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(u => u.Email).NotNull().NotEmpty();
+            RuleFor(u => u.FirstName).NotNull().NotEmpty();
+            RuleFor(u => u.LastName).NotNull().NotEmpty();
+            RuleFor(u => u.Password).NotNull().NotEmpty();
+        }
+    }
+}
